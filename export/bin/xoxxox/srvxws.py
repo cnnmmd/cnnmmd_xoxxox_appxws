50f529e5e34c0a5f0209723fd870a56878c540ff
azsgit <10238943+azsgit@users.noreply.github.com>
2026-07-05 22:28:35 +0900

    allow server to receive scripts

--- export/bin/xoxxox/srvxws.py
+++ export/bin/xoxxox/srvxws.py
@@ -9,18 +9,40 @@ from xoxxox.shared import Custom
 #---------------------------------------------------------------------------
 
 async def ressys(datreq):
-  global diccnf, dicprm, prcsub
-  dicreq = await datreq.json()
-  diccnf = Custom.update(dicreq["config"], dicprm)
-  if prcsub and prcsub.returncode is None:
-    prcsub.terminate()
-    await prcsub.wait()
-  if diccnf["script"] != "":
-    prcsub = await asyncio.create_subprocess_exec(
-      "/opt/common/bin/xoxxox/runapp.sh", diccnf["usrapp"], diccnf["envapp"], diccnf["script"]
-    )
+  global diccnf, dicprm, prcsub, sectmo
+  try:
+    dicres={"status": "1"}
+    dicreq = await datreq.json()
+
+    if 'config' in dicreq:
+      diccnf = Custom.update(dicreq["config"], dicprm)
+    else:
+      diccnf["usrapp"] = dicreq["usrapp"]
+      diccnf["envapp"] = dicreq["envapp"]
+      diccnf["script"] = dicreq["script"]
+
+    if prcsub and prcsub.returncode is None:
+      prcsub.terminate()
+      await prcsub.wait()
+    if diccnf["script"] != "":
+      prcsub = await asyncio.create_subprocess_exec(
+        "/opt/common/bin/xoxxox/runapp.sh", diccnf["usrapp"], diccnf["envapp"], diccnf["script"],
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+      )
+      try:
+        stdout, stderr = await asyncio.wait_for(
+          prcsub.communicate(),
+          timeout=sectmo,
+        )
+        if prcsub.returncode != 0:
+          dicres={"status": "0", "errors": stderr.decode()}
+      except asyncio.TimeoutError:
+        pass
+  except Exception as e:
+    dicres={"status": "0", "errors": str(e)}
   return web.Response(
-    text=json.dumps({"status": "1"}),
+    text=json.dumps(dicres),
     content_type="application/json",
   )
 
@@ -29,7 +51,6 @@ async def ressys(datreq):
 parser = argparse.ArgumentParser()
 parser.add_argument("--secure", default="0")
 parser.add_argument("--svport", type=int, default="80")
-parser.add_argument("--config")
 parser.add_argument("--adraco", type=str) # default: cnfnet
 parser.add_argument("--pthcrt", type=str) # default: cnfnet
 parser.add_argument("--pthkey", type=str) # default: cnfnet
@@ -48,6 +69,7 @@ adrsys = "/sys"
 
 #---------------------------------------------------------------------------
 
+sectmo = 5 # タイムアウト（正常に起動と判断するまでの時間）
 diccnf = {}
 prcsub = None
 appweb = web.Application()
